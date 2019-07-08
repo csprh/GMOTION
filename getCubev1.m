6d4a5ab7d109d27c958a1b7905f52c0566e9eb4f
csprh <paul.hill@bristol.ac.uk>
2019-07-08 21:41:35 +0100

    update

---
+++ getCubev1.m
@@ -0,0 +1,64 @@
+load  UKEQs2016-2018 
+
+worldmap([49 59],[-12 4]) 
+geoshow('landareas.shp') 
+
+DScale = 500000;
+Latitude0 = Latitude(ML==0); Longitude0 = Longitude(ML==0); thisDate0 = thisDate(ML==0);
+Latitude1 = Latitude(ML~=0); Longitude1 = Longitude(ML~=0); thisDate1 = thisDate(ML~=0);
+
+scaleD0 = (thisDate0 - min(thisDate0(:)))./(max(thisDate0(:))-min(thisDate0(:)));
+hold on;
+view(3)
+
+cubeLenL = 0.15;
+cubeLenD = 0.025;
+Longitude0S = Longitude0(40);
+Latitude0S = Latitude0(40);
+scaleD0S = scaleD0(40);
+
+lat0 = Latitude0S-cubeLenL; lat1 = Latitude0S+cubeLenL;
+lon0 = Longitude0S-cubeLenL; lon1 = Longitude0S+cubeLenL;
+
+lonAll = h5read('030A_03647_101313-vel.h5','/Longitude');
+latAll = h5read('030A_03647_101313-vel.h5','/Latitude');
+dateAll = h5read('030A_03647_101313-vel.h5','/Dates');
+
+[~, minIndLon] = min(abs(lonAll-lon0));
+[~, maxIndLon] = min(abs(lonAll'-lon1));
+[~, minIndLat] = min(abs(latAll-lat0));
+[~, maxIndLat] = min(abs(latAll'-lat1));
+
+minIndLon = min(minIndLon);
+maxIndLon = max(maxIndLon);
+minIndLat = min(minIndLat);
+maxIndLat = max(maxIndLat);
+
+startL = [minIndLon minIndLat]; countL = [maxIndLon-minIndLon maxIndLat-minIndLat];
+startLC = [startL 1]; countLC = [countL 170];
+lonThis = h5read('030A_03647_101313-vel.h5','/Longitude', startL, countL);
+latThis = h5read('030A_03647_101313-vel.h5','/Latitude', startL, countL);
+
+
+%cd1 = h5read('030A_03647_101313-vel.h5','/Cumulative_Displacement',start,count);
+cThis = h5read('030A_03647_101313-vel.h5','/Cumulative_Displacement_TSmooth',startLC,countLC);
+cThis_100 = cThis(:,:,100);
+thisInd2 = isnan(cThis_100);
+lon2 = lonThis(~thisInd2);
+lat2 = latThis(~thisInd2);
+allTriplets  = [];
+for ii = 2:170
+
+	cThisFrame = cThis(:,:,ii);
+
+	thisInd1 = isnan(cThisFrame);
+    cThisFrameNoNaN = cThisFrame(~thisInd1);
+    thisDate = dateAll(ii);
+    thisTriplet = [lon2; lon2; thisDate*ones(size(lon2); cThisFrameNoNaN];
+    allTriplets = [thisTriplet allTriplets];
+end	
+
+noOfPoints = size(allTriplets,1);
+for ii = 2:170
+end
+
