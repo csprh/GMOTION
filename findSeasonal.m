b4c98c4bb09908be6d155c45b12c52b98aa14f43
csprh <paul.hill@bristol.ac.uk>
2019-08-03 20:53:21 +0100

    update

--- findSeasonal.m
+++ findSeasonal.m
@@ -3,12 +3,19 @@ frameName = '030A_03647_101313-vel.h5';
 
 dateAll = h5read(frameName,'/Date');
 
+theseDates = datenum(num2str(dateAll),'yyyymmdd');
+days = theseDates(end)-theseDates(end-1);
+interpDates = theseDates(1):days:theseDates(end);
+
+
+interpOut = interp1(theseDates,thesePoints, interpDates);
+
 
 start = [41809 4012 1];
 count = [44381-41809 4537-4012 170];
 
 
-%cd1 = h5read(frameName,'/Cumulative_Displacement',start,count);
+cd1 = h5read(frameName,'/Cumulative_Displacement',start,count);
 cdts = h5read(frameName,'/Cumulative_Displacement_TSmooth',start,count);
 lon1 = h5read(frameName,'/Longitude', start(1:2), count(1:2));
 lat1 = h5read(frameName,'/Latitude', start(1:2), count(1:2));
@@ -21,6 +28,10 @@ for ii = 2:170
 	this_cd1 = cd1(:,:,ii);
 	thisInd0 = isnan(this_cdts);
 	thisInd1 = isnan(this_cd1);
+    
+    for iii = 1:length(thisInd0)
+        thesePoints = thisInd0(:);
+    end
 	this_cdts_1D(:,ii-1)= this_cdts(~thisInd0);
 	this_cd1_1D(:,ii-1)= this_cd1(~thisInd1);
 end	
