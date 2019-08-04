b938b36810b7908256f9645e3476d8981e417c8c
csprh <paul.hill@bristol.ac.uk>
2019-08-04 14:30:52 +0100

    update

--- findSeasonal2.m
+++ findSeasonal2.m
@@ -8,12 +8,15 @@ daysInYear = 365.25;
 lagAC = round(daysInYear/daysBetweenSamples);
 %dateAll = h5read(frameName,'/Date');
 
-threshAC = 0.5;
+threshAC0 = 0.2;
+threshAC1 = 0.15;
+threshAC2 = 0.1;
+threshAC3 = 0.05;
 load stokeData_cdts2
 
-signal1D = cd_1D;
+%signal1D = cd_1D;
 %signal1D = cdTSmooth_1D;
-%signal1D = cdAPS_1D;
+signal1D = cdAPS_1D;
 %signal1D = cdFilt_1D;
 
 for ii = 1:size(cd_1D,1)
@@ -29,8 +32,15 @@ for ii = 1:size(cd_1D,1)
     %> threshAC;
 end
 
-arrayACInd = arrayAC>threshAC;
+arrayACInd0 = arrayAC>threshAC0;
+arrayACInd1 = arrayAC>threshAC1;
+arrayACInd2 = arrayAC>threshAC2;
+arrayACInd3 = arrayAC>threshAC3;
 
-this_signal1DInd = signal1D(arrayACInd,:);
-save arrayAC arrayAC this_signal1DInd 
+this_signal1DInd0 = signal1D(arrayACInd0,:);
+this_signal1DInd1 = signal1D(arrayACInd1,:);
+this_signal1DInd2 = signal1D(arrayACInd2,:);
+this_signal1DInd3 = signal1D(arrayACInd3,:);
+
+save arrayAC arrayAC this_signal1DInd0 this_signal1DInd1 this_signal1DInd2 this_signal1DInd3 
 
