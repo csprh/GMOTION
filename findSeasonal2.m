35a117af98771dfd2962d59998985940a4957a30
csprh <paul.hill@bristol.ac.uk>
2019-08-04 13:33:21 +0100

    update

---
+++ findSeasonal2.m
@@ -0,0 +1,25 @@
+load  UKEQs2016-2018
+frameName = '030A_03647_101313-vel.h5';
+
+
+
+
+%dateAll = h5read(frameName,'/Date');
+
+threshAC = 10;
+load stokeData_cdts2
+
+for ii = 1:size(cd_1D,1)
+    this_cd = cd_1D(ii,:);
+	this_cdTSmooth = cdTSmooth_1D(ii,:);
+	this_cdAPS = cdAPS_1D(ii,:);
+    this_cdFilt = cdFilt_1D(ii,:);
+    
+    thisCD = this_cdAPS;
+    ac = autocorr(thisCD);
+    arrayAC(ii) =  abs(ac(61));
+    %> threshAC;
+end
+
+save arrayAC arrayAC
+
