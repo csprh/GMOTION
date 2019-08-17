e33bc53b60c7a2f7315bd2af03fa16dfb0d6bf5e
csprh <paul.hill@bristol.ac.uk>
2019-08-18 00:11:52 +0100

    Update

--- EQs/interpRegion.m
+++ EQs/interpRegion.m
@@ -49,41 +49,45 @@ minIndLat = min(b);maxIndLat = max(b);
 startL = [minIndLon minIndLat]; countL = [maxIndLon-minIndLon maxIndLat-minIndLat];
 startLC = [startL 1]; countLC = [countL lenD];
 
-cdOrig = h5read(frameName,'/Cumulative_Displacement',startLC,countLC);
+%cdOrig = h5read(frameName,'/Cumulative_Displacement',startLC,countLC);
 cdTSmooth = h5read(frameName,'/Cumulative_Displacement_TSmooth',startLC,countLC);
-cdAPS = h5read(frameName,'/Cumulative_Displacement_APS',startLC,countLC);
-cdFilt = h5read(frameName,'/Cumulative_Displacement_Filt',startLC,countLC);
+%cdAPS = h5read(frameName,'/Cumulative_Displacement_APS',startLC,countLC);
+%cdFilt = h5read(frameName,'/Cumulative_Displacement_Filt',startLC,countLC);
 lon1 = h5read(frameName,'/Longitude', startLC(1:2), countLC(1:2));
 lat1 = h5read(frameName,'/Latitude', startLC(1:2), countLC(1:2));
-cd1_100 = cdOrig(:,:,100);
+cd1_100 = cdTSmooth(:,:,100);
 thisInd2 = isnan(cd1_100);
 
 lon2 = lon1(~thisInd2);
 lat2 = lat1(~thisInd2);
 
-cdOrig = flattenData(cdOrig, lenD, ~thisInd2);
+%cdOrig = flattenData(cdOrig, lenD, ~thisInd2);
 cdTSmooth = flattenData(cdTSmooth, lenD, ~thisInd2);
-cdAPS = flattenData(cdAPS,lenD, ~thisInd2);
-cdFilt = flattenData(cdFilt, lenD, ~thisInd2);
-
-for ii = 1:length(cdOrig)
-        thisTScd = cdOrig(ii,:); thisTScd = thisTScd(:);
+%cdAPS = flattenData(cdAPS,lenD, ~thisInd2);
+%cdFilt = flattenData(cdFilt, lenD, ~thisInd2);
+outcdTSmooth = [];
+for ii = 1:length(cdTSmooth)
+        %thisTScd = cdOrig(ii,:); thisTScd = thisTScd(:);
         thisTScdTSmooth = cdTSmooth(ii,:); thisTScdTSmooth = thisTScdTSmooth(:);
-        thisTScdAPS = cdAPS(ii,:); thisTScdAPS = thisTScdAPS(:);
-        thisTScdFilt = cdFilt(ii,:); thisTScdFilt = thisTScdFilt(:);
-        outcd(ii,:) = interp1(theseDates,thisTScd, interpDates);
+        %thisTScdAPS = cdAPS(ii,:); thisTScdAPS = thisTScdAPS(:);
+        %thisTScdFilt = cdFilt(ii,:); thisTScdFilt = thisTScdFilt(:);
+        %outcd(ii,:) = interp1(theseDates,thisTScd, interpDates);
         outcdTSmooth(ii,:) = interp1(theseDates,thisTScdTSmooth, interpDates);
-        outcdAPS(ii,:) = interp1(theseDates,thisTScdAPS, interpDates);
-        outcdFilt(ii,:) = interp1(theseDates,thisTScdFilt, interpDates);
+        %outcdAPS(ii,:) = interp1(theseDates,thisTScdAPS, interpDates);
+        %outcdFilt(ii,:) = interp1(theseDates,thisTScdFilt, interpDates);
 end
 
 out.lon2 = lon2;
 out.lat2 = lat2;
-out.outcd = outcd;
+out.outcd = [];
 out.outcdTSmooth =outcdTSmooth;
-out.outcdAPS =outcdAPS;
-out.outcdFilt =outcdFilt;
+out.outcdAPS = [];
+out.outcdFilt = [];
 out.interpDates = interpDates;
+out.outcd = outcd;
+%out.outcdAPS =outcdAPS;
+%out.outcdFilt =outcdFilt;
+
 
 function cdOrig2 = flattenData(cdOrig, lenD, nNaNInd)
 for ii = 1:lenD
