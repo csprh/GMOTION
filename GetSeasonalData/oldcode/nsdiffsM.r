5a4102e0c562323a256f23ce51a76e7523417fce
csprh <paul.hill@bristol.ac.uk>
2019-12-10 19:55:49 +0000

    update

--- GetSeasonalData/oldcode/nsdiffsM.r
+++
@@ -1,9 +0,0 @@
-require(forecast)
-rawdat <- read.csv('in.csv',header=FALSE)
-X <- as.numeric(rawdat)
-for (i in 1:8){
-    cat(i)
-    print(paste("The year is", i))
-    data <-nsdiffs(ts(X,frequency = 61))
-}
-write.csv(data,'out.csv')
